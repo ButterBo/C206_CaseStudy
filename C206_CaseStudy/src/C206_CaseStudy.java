33e7237520d182851ad4c3a5dacefd360e8f1fa9
ButtorBo <21044912@myrp.edu.sg>
2022-08-04 22:10:44 +0800

    Added some more option to caseStudy

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -7,17 +7,34 @@ public class C206_CaseStudy {
 		
 		int option = 0;
 		
-		while (option != -1) {
+		while (option != 9) {
+			C206_CaseStudy.startmenu();
+			option = Helper.readInt("Enter an option: ");
 			
+			if (option == 1) {
+				
+			} else if (option == 2) {
+				
+			} else if (option == 9) {
+				System.out.println("Bye bye, have a nice day");
+			} else {
+				System.out.println("Invalid option");
+			}
 		}
 
 	}
 	
 	public static void startmenu() {
-		System.out.println("RESOURCE CENTRE APP");
-		Helper.line(80, "-");
+		C206_CaseStudy.setHeader("RESOURCE CENTRE APP");
+		Helper.line(80, "=");
 		System.out.println("1. Login");
 		System.out.println("2. Register");
+		Helper.line(80, "=");
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
 		Helper.line(80, "-");
 	}
 
