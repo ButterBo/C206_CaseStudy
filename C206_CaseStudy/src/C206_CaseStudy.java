b6020267e1a3e6bfa2cca9158ca012ebeefadf65
ButtorBo <21044912@myrp.edu.sg>
2022-08-04 20:46:29 +0800

    Not much

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,8 +1,15 @@
-
+import java.util.ArrayList;
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<CCA> CCAList = new ArrayList<CCA>();
+		
+		int option = 0;
+		
+		while (option != -1) {
+			
+		}
 
 	}
 
